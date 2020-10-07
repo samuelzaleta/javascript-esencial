5b8422ea605181ac5ec41f77a17a4c7002c11b81
Samuel Zaleta <samuel.zaleta.cbtis103@gmail.com>
2020-10-06 19:42:48 -0500

    practica 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-alert('hola desde un archivo externo')
+"use strict" //siempre tener el modo estricto es lo recomendable
+
+//alert('hola desde un archivo externo')
+
+//Comentario de una linea
+//Es te es un mensaje de la consola
+//console.log('hola consola')
+/*
+ Comentario de de bloque
+*/
+
+//las variables son de tipos destructivas
+var nombre ="samuel"; //no olvidar el punto y coma
+
+console.log(nombre)
+
+function saludo(){
+    let nombre ="samuel"; //let es un contenedor de bloque 
+        console.log(nombre)
+    let edad = 34;
+    console.log(edad)
+}
+saludo();
+
+//contantes
+
+const pi = 3.1416; //esta no permite que el valor cambie
+
+
+
+
