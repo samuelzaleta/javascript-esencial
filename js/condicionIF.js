63324755ea5efa454ac8973fff7b01937f762462
Samuel Zaleta <samuel.zaleta.cbtis103@gmail.com>
2020-10-14 13:28:04 -0500

    hola mundo

---
+++ js/condicionIF.js
@@ -0,0 +1,15 @@
+"use strict"
+var dato1 = 100;
+var dato2 = 20;
+var resultados  = 'Sin datos';
+
+if (dato1 > dato2) {
+    resultados = 'la condición se cumplio';
+} else{
+    resultados = 'La condición no e cumplio';
+}
+console.log(resultados)
+
+
+
+
